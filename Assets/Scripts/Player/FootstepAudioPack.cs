fce7baa6d36efa5f6b63f34069dddfb0f50778e6
Sheehan Ahmed <ssahmed@usc.edu>
2020-02-01 15:35:38 -0800

    asdf

---
+++ Assets/Scripts/Player/FootstepAudioPack.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepAudioPack : ScriptableObject
+{
+
+    public AudioClip[] leftFootsteps;
+    public AudioClip[] rightFootsteps;
+
+}
---
+++ Assets/Scripts/Player/FootstepManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepManager : MonoBehaviour
+{
+
+    FootstepAudioPack currentAudioPack;
+
+    AudioSource source;
+
+    bool leftFootForward;
+    int index;
+    public void TakeFootstep()
+    {
+        leftFootForward = !leftFootForward;
+
+        if(leftFootForward)
+        {
+            index++;
+            index = index % 3;
+        }
+
+        if(leftFootForward)
+        {
+            source.clip = currentAudioPack.leftFootsteps[index];
+        } else if(leftFootForward)
+        {
+            source.clip = currentAudioPack.rightFootsteps[index];
+        }
+
+        source.Play();
+
+    }
+
+}

ca356bcffb7cb12400ed42983c6fdf254d730d89
Sheehan Ahmed <ssahmed@usc.edu>
2020-02-01 04:24:55 -0800

    asfd

---
+++ Assets/Scripts/Audio/AudioMuffler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioLowPassFilter))]
+public class AudioMuffler : MonoBehaviour
+{
+
+    public Transform player;
+
+    AudioLowPassFilter filter;
+
+    bool obstacle = false;
+
+    private void Start()
+    {
+        filter = GetComponent<AudioLowPassFilter>();
+    }
+    private void Update()
+    {
+        obstacle = true;
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, player.position - transform.position);
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.collider.CompareTag("Player"))
+            {
+                obstacle = false;
+            }
+        }
+
+        filter.cutoffFrequency = Mathf.Lerp(filter.cutoffFrequency, obstacle ? 3500 : 20000, 5.0f * Time.deltaTime);
+    }
+}

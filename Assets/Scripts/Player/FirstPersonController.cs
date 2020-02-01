a54b713f521061a965be3b1f9fae88da361e33de
Sheehan Ahmed <ssahmed@usc.edu>
2020-01-31 19:52:11 -0800

    first push

---
+++ Assets/Scripts/Player/FirstPersonController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonController : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class MovementVariables
+    {
+        public float speed = 10f;
+        public float smoothing = 3f;
+        public Vector2 motionInput;
+        public Vector3 velocityVector;
+        public bool crouching;
+        public float crouchingSpeedMultiplier = .5f;
+    }
+
+    [System.Serializable]
+    public class TurningVariables
+    {
+        public Vector2 mouseInput;
+        public Vector2 viewAngle;
+        public float sensitivity = 1.0f;
+        public bool invertY = false;
+    }
+
+    [SerializeField] private MovementVariables movementVariables;
+    [SerializeField] private TurningVariables turningVariables;
+
+
+    public Camera camera;
+
+    private CharacterController characterController;
+
+
+    private void Update()
+    {
+        GatherInput();
+        Locomotion();
+        TurningAndCamera();
+    }
+
+    private void GatherInput()
+    {
+        movementVariables.motionInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Horizontal")).normalized;
+        turningVariables.mouseInput = new Vector2(Input.GetAxisRaw("MouseX"), (turningVariables.invertY ? 1.0f : -1.0f) * Input.GetAxisRaw("MouseY"));
+        if (Input.GetButtonDown("ToggleCrouch")) {
+            movementVariables.crouching = !movementVariables.crouching;
+        }
+    }
+
+    private void Locomotion()
+    {
+        Vector3 targetVelocity =
+            (movementVariables.motionInput.x * transform.right + movementVariables.motionInput.y * transform.forward)
+            * movementVariables.speed * (movementVariables.crouching ? movementVariables.crouchingSpeedMultiplier : 1.0f);
+
+        movementVariables.velocityVector =
+            Vector3.Lerp(movementVariables.velocityVector, targetVelocity, movementVariables.smoothing * Time.deltaTime);
+
+        characterController.SimpleMove(movementVariables.velocityVector);
+    }
+
+    private void TurningAndCamera()
+    {
+        turningVariables.viewAngle += turningVariables.mouseInput * turningVariables.sensitivity;
+        transform.localRotation = Quaternion.Euler(Vector3.up * turningVariables.viewAngle.x);
+        camera.transform.localRotation = Quaternion.Euler(Vector3.right * turningVariables.viewAngle.y);
+        camera.transform.localPosition = Vector3.up * (movementVariables.crouching ? 0.0f : 1.0f);
+    }
+
+}

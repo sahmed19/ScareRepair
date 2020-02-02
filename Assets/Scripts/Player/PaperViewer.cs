f8bbcb4a01467312126416ca21d4792d229bb05d
Sheehan Ahmed <ssahmed@usc.edu>
2020-02-01 16:37:18 -0800

    asdf

---
+++ Assets/Scripts/Player/PaperViewer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperViewer : MonoBehaviour
+{
+
+    public AnimationCurve drawCurve;
+
+    float drawn = 0f;
+
+    private void Update()
+    {
+
+        if (Input.GetButton("ShowPaper"))
+        {
+            drawn += Time.deltaTime * 3.0f;
+        } else
+        {
+            drawn -= Time.deltaTime * 3.0f;
+        }
+
+        drawn = Mathf.Clamp01(drawn);
+
+        transform.localEulerAngles = Vector3.right * 90f * (1f-drawCurve.Evaluate(drawn));
+
+    }
+
+}

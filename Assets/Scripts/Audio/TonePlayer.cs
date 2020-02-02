cc0cf79f4599e9996516471c3b480be209b02735
Sheehan Ahmed <ssahmed@usc.edu>
2020-02-02 08:06:16 -0800

    asdf

---
+++ Assets/Scripts/Audio/TonePlayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TonePlayer : MonoBehaviour
+{
+
+    public AudioClip tone;
+    public AudioClip toneEnd;
+
+    public AudioSource source;
+
+    bool playing = false;
+    float timer = 0f;
+
+    public void StartTone()
+    {
+        source.clip = tone;
+        source.Play();
+        playing = true;
+    }
+
+    private void Update()
+    {
+        if(playing)
+        {
+            timer += Time.deltaTime;
+            if (timer > tone.length - 2.0f)
+            {
+                EndTone();
+            }
+        } else
+        {
+            timer = 0f;
+        }
+    }
+
+    public void EndTone()
+    {
+        if(!playing)
+        {
+            return;
+        }
+        source.clip = toneEnd;
+        source.Play();
+        playing = false;
+    }
+
+}
---
+++ Assets/Scripts/Sequence/ShowSubtitle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowSubtitle : MonoBehaviour
+{
+
+    public TextMeshProUGUI subtitle;
+
+    public void ShowSub(string sub)
+    {
+        StartCoroutine(ShowSubCoroutine(sub));
+    }
+
+    IEnumerator ShowSubCoroutine(string sub)
+    {
+
+        yield return new WaitForSeconds(3.0f);
+        subtitle.text = sub;
+
+        float t = 5.0f;
+
+        while(t > 0f)
+        {
+            t -= Time.deltaTime;
+            subtitle.color = new Color(1f, 1f, 1f, .5f * Mathf.Clamp01(t));
+            yield return null;
+        }
+
+    }
+
+}
